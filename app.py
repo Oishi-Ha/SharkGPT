a0762cf8c385ace47fe57c5af82d4878559f533c
Oishi-Ha <oishi_ha@163.com>
2023-06-13 15:04:33 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+import openai
+from flask import Flask, render_template, request
+from langchain.llms import OpenAI
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route('/', methods=("GET", "POST"))
+def index():
+    return render_template("index.html")
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    prompt = request.form['message']
+
+    # Llms
+    llm = OpenAI(temperature=0.9)
+
+    # Show stuff to the screen if there's prompts
+    if prompt:
+        response = llm(prompt)
+    return response
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
