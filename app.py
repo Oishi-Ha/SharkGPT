5d674527f7fda6dee2a830d110266db594dd68bd
Oishi-Ha <oishi_ha@163.com>
2023-06-13 21:29:00 +0800

    init commit

--- app.py
+++ app.py
@@ -10,26 +10,26 @@ load_dotenv(env_path)
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
+
 @app.route('/', methods=("GET", "POST"))
 def index():
     return render_template("index.html")
 
+
 @app.route('/chat', methods=['POST'])
 def chat():
     prompt = request.form['message']
-    if not prompt:
-        return jsonify({'error': 'No message provided'}), 400
-
-    # Llms
     llm = OpenAI(model_name="text-davinci-003", temperature=0.9)
     response = llm.generate([prompt], api_key=openai_api_key)
 
-    # Ensure the response is in a string format
-    if isinstance(response, str):
-        return response
-    else:
-        return jsonify(response)  # Convert the response to a JSON string
+    # Extract the properties from the response
+    response_dict = {
+        'generations': response.generations,
+        'llm_output': response.llm_output,
+        'run': response.run,
+    }
+    return jsonify(response_dict)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
