fd5b81971abec010e5309e1f030ed3d0f0785dcd
Oishi-Ha <oishi_ha@163.com>
2023-06-13 21:45:10 +0800

    init commit

--- app.py
+++ app.py
@@ -11,6 +11,30 @@ load_dotenv(env_path)
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 
+def generation_to_dict(generation):
+    # Assuming that 'generation' has the properties 'id', 'text', and 'tokens'
+    return {
+        "id": generation.id,
+        "text": generation.text,
+        "tokens": generation.tokens,
+    }
+
+
+def runinfo_to_dict(runinfo):
+    # Assuming that 'runinfo' has the property 'run_id'
+    return {
+        "run_id": runinfo.run_id,
+    }
+
+
+def llmresult_to_dict(llmresult):
+    return {
+        "generations": [generation_to_dict(g) for g in llmresult.generations],
+        "llm_output": llmresult.llm_output,
+        "run": runinfo_to_dict(llmresult.run) if llmresult.run else None,
+    }
+
+
 @app.route('/', methods=("GET", "POST"))
 def index():
     return render_template("index.html")
@@ -22,7 +46,7 @@ def chat():
     llm = OpenAI(model_name="text-davinci-003", temperature=0.9)
     response = llm.generate([prompt], api_key=openai_api_key)
 
-    return response
+    return jsonify(llmresult_to_dict(response))
 
 
 if __name__ == '__main__':
