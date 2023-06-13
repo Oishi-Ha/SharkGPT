eb23b5dab67d6d3f89dad8e0d6d6cb602969fac2
Oishi-Ha <oishi_ha@163.com>
2023-06-13 18:15:07 +0800

    init commit

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ import os
 # import openai
 from flask import Flask, render_template, request
 from langchain.llms import OpenAI
+from dotenv import load_dotenv
+from pathlib import Path
 
 app = Flask(__name__)
+# 获取 .env 文件的路径
+env_path = Path(__file__).resolve().parent.parent / ".env"
+
+# 加载环境变量
+load_dotenv(env_path)
+
 openai_api_key = os.getenv("OPENAI_API_KEY")
+print(openai_api_key)
 
 
 @app.route('/', methods=("GET", "POST"))
