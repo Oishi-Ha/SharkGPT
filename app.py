f9d38ea3a338627e7d03a134ccd13cac3fc51b0f
Oishi-Ha <oishi_ha@163.com>
2023-06-13 15:19:13 +0800

    init commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 from langchain.llms import OpenAI
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 
 @app.route('/', methods=("GET", "POST"))
@@ -17,7 +17,7 @@ def chat():
     prompt = request.form['message']
 
     # Llms
-    llm = OpenAI(temperature=0.9)
+    llm = OpenAI(openai_api_key='your_api_key', temperature=0.9)
 
     # Show stuff to the screen if there's prompts
     if prompt:
